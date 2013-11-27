2304963f87ae1a40c28fcef7071980ea5d3db9e0
JamesGraham <jamesgraham09@gmail.com>
2013-11-27 09:30:57 +0000

    funky bikes with names and colours

--- lib/station.rb
+++ lib/station.rb
@@ -27,19 +27,3 @@ class DockingStation
 end
 
 
-require_relative '../lib/bikes.rb'
-
-
-station = DockingStation.new
-station.dock(Bike.new("penny farthing"))
-station.dock(Bike.new)
-station.dock(Bike.new("live strong"))
-
-puts "There are currently #{station.bike_count} bikes here"
-
-bike = station.bikes.select { |bike| bike.name == "live strong"}.first
-puts bike.colour
-bike.respray("yellow")
-puts bike.colour
-
-station.bikes.each { |bike| puts "#{bike.name}, #{bike.broken}, #{bike.colour}"}
